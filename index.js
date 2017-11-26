1a8ff11122119f5cd707bb2eb9da4fa1163ad3bd
Sean Tracey <seanmtracey@gmail.com>
2017-11-26 15:27:42 +0000

    Tidy up the code a little. Fix the array that remembers everything.

--- index.js
+++ index.js
@@ -8,103 +8,94 @@ const imageSize = require('image-size');
 
 const TMP_PATH = process.env.TMP_PATH || '/tmp';
 
-// Based on https://github.com/garyshort/chickens
-let Clusters = [];
+function doTheThing(options){ 
 
-let NeighbourThreshold = 3;
-let MergeThreshold = 10;
-let NoiseThreshold = 2;
+    // Based on https://github.com/garyshort/chickens
+    let Clusters = [];
 
-function EuclideanDistanceBetween(p1, p2){
-    var a = p1[0] - p2[0]
-    var b = p1[1] - p2[1];
+    let NeighbourThreshold = 3;
+    let MergeThreshold = 10;
+    let NoiseThreshold = 2;
 
-    return Math.sqrt( a * a + b * b );
-}
+    function EuclideanDistanceBetween(p1, p2){
+        var a = p1[0] - p2[0]
+        var b = p1[1] - p2[1];
 
-function IsCloseTo(p1, p2){
-    return EuclideanDistanceBetween(p1, p2) <= NeighbourThreshold;
-}
+        return Math.sqrt( a * a + b * b );
+    }
 
-function CalculateVoteOfPoint(p1, p2){
-    return 1 / EuclideanDistanceBetween(p1, p2);
-}
+    function IsCloseTo(p1, p2){
+        return EuclideanDistanceBetween(p1, p2) <= NeighbourThreshold;
+    }
 
-function CullNoiseClusters(noiseThreshold = NoiseThreshold){
-    return Clusters.filter(cluster => cluster.length > noiseThreshold);
-}
+    function CalculateVoteOfPoint(p1, p2){
+        return 1 / EuclideanDistanceBetween(p1, p2);
+    }
 
-function ClusterPoint(p){
-    let chosenCluster = null;
-    let votesCast = 0;
+    function CullNoiseClusters(noiseThreshold = NoiseThreshold){
+        return Clusters.filter(cluster => cluster.length > noiseThreshold);
+    }
 
-    // If this is the first point, it's the root of the first cluster
-    if (Clusters.length == 0) {
-        let l = [];
-        l.push(p);
-        Clusters.push(l);
-    } else {
-        // Otherwise iterate over all the clusters...
-        Clusters.forEach(cluster => {
-            // Find all the points within PointThreshold distance
+    function ClusterPoint(p){
+        let chosenCluster = null;
+        let votesCast = 0;
 
-            let votingPoints = cluster.filter(point => {
-                return IsCloseTo(point, p)
-            });
-
-            // Sum the votes of the voting points
-            // let totalVotes = votingPoints.AsParallel().Sum( aPoint => CalculateVoteOfPoint(aPoint, p) );
-            let totalVotes = votingPoints.reduce(function (totalSoFar, point) { return CalculateVoteOfPoint(point, p) }, 0);
-
-            // If this is the current max then this is the selected cluster
-            if (totalVotes > votesCast) {
-                chosenCluster = cluster;
-                votesCast = totalVotes;
-            }
-        });
-
-        // After voting if there's a chosen cluster, add the point
-        if (chosenCluster != null){
-            chosenCluster.push(p);
-        } else {
-            // There's no close clusters, so start a new one
+        // If this is the first point, it's the root of the first cluster
+        if (Clusters.length == 0) {
             let l = [];
             l.push(p);
             Clusters.push(l);
+        } else {
+            // Otherwise iterate over all the clusters...
+            Clusters.forEach(cluster => {
+                // Find all the points within PointThreshold distance
+
+                let votingPoints = cluster.filter(point => {
+                    return IsCloseTo(point, p)
+                });
+
+                // Sum the votes of the voting points
+                let totalVotes = votingPoints.reduce(function (totalSoFar, point) { return CalculateVoteOfPoint(point, p) }, 0);
+
+                // If this is the current max then this is the selected cluster
+                if (totalVotes > votesCast) {
+                    chosenCluster = cluster;
+                    votesCast = totalVotes;
+                }
+            });
+
+            // After voting if there's a chosen cluster, add the point
+            if (chosenCluster != null){
+                chosenCluster.push(p);
+            } else {
+                // There's no close clusters, so start a new one
+                let l = [];
+                l.push(p);
+                Clusters.push(l);
+            }
         }
     }
-}
 
-function ClusterAllPoints(dimensions, imageData){
+    function ClusterAllPoints(dimensions, imageData){
 
-    let clustered = 0;
-    const total = dimensions.width * dimensions.height;
+        let clustered = 0;
+        const total = dimensions.width * dimensions.height;
+        
+        for(var zz = 0; zz < imageData.length; zz += 4){
+
+            var x = (zz / 4) % dimensions.width;
+            var y = Math.floor((zz / 4) / dimensions.width);
+
+            if(imageData[zz] === 255){
+                ClusterPoint([x, y]);
+            }
+
+        }
+
+    }
+
+    const outputPath = `${__dirname}/${uuid()}.jpg`    
     
-    for(var zz = 0; zz < imageData.length; zz += 4){
-
-        var x = (zz / 4) % dimensions.width;
-        var y = Math.floor((zz / 4) / dimensions.width);
-
-        if(imageData[zz] === 255){
-            ClusterPoint([x, y]);
-        }
-
-    }
-
-}
-
-module.exports = function(options){
-
-    if(!options.path){
-        return Promise.reject();
-    }
-
-    NeighbourThreshold = options.neighbourThreshold || 3;
-    MergeThreshold = options.mergeThreshold || 10;
-    NoiseThreshold = options.noiseThreshold || 2;
-
-    const outputPath = `${__dirname}/${uuid()}.jpg`
-
     return new Promise( (resolve, reject) => {
         
         let process = gm(options.path)
@@ -150,5 +141,19 @@ module.exports = function(options){
         ;
     
     });
+
+}
+
+module.exports = function(options){
+
+    if(!options.path){
+        return Promise.reject();
+    }
+
+    NeighbourThreshold = options.neighbourThreshold || 3;
+    MergeThreshold = options.mergeThreshold || 10;
+    NoiseThreshold = options.noiseThreshold || 2;
+   
+    return doTheThing(options);
 
 }
